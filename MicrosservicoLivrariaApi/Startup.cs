24d46d31d92c06c92ec4c981da45486d0f3b5ae3
Carina Roriz Pimenta <carinaroriz@hotmail.com>
2019-04-15 23:00:40 -0300

    Ajuste do nome do projeto no swagger

--- MicrosservicoLivrariaApi/Startup.cs
+++ MicrosservicoLivrariaApi/Startup.cs
@@ -32,7 +32,7 @@ namespace MicrosservicoMicrosservicoApi
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info() { Title = "MicrosservicoApi", Version = "v1" });
+                c.SwaggerDoc("v1", new Info() { Title = "MicrosservicoLivrariaApi", Version = "v1" });
                 // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, "MicrosservicoLivrariaApi.xml");
